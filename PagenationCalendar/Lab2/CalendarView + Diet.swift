1611b1ff18b3d377f4c4325f2e72275984c07d42
심상갑 <kappa2410@gmail.com>
2026-02-23 17:15:16 +0900

    Start: dietCardAddBtn 시작

--- PagenationCalendar/Lab2/CalendarView + Diet.swift
+++ PagenationCalendar/Lab2/CalendarView + Diet.swift
@@ -14,12 +14,12 @@ extension CalendarView {
     var dietHistoryList: some View {
         VStack(spacing: 12) {
             dietHistoryTitle
-            dietCardList
+            dietCardAddBtn
+//            dietCardList
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
-
 
 extension CalendarView {
     @ViewBuilder
@@ -30,6 +30,30 @@ extension CalendarView {
             
             Spacer()
         }
+        .padding(.horizontal, 16)
+    }
+}
+
+extension CalendarView {
+    @ViewBuilder
+    var dietCardAddBtn: some View {
+        Button {
+            
+        } label: {
+            VStack(spacing: 10) {
+                Text("오늘 먹은 음식을 기록해 보세요")
+                    .font(.system(size: 16, weight: .semibold))
+                    .foregroundStyle(Color(hex: "6e7881"))
+                    .multilineTextAlignment(.center)
+                
+                Image("img-add-empty")
+            }
+            .frame(height: 112)
+            .frame(maxWidth: .infinity)
+        }
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+        .shadow(color: Color(hex: "14121416"), radius: 10, x: 0, y: 1)
         .padding(.horizontal, 16)
     }
 }
